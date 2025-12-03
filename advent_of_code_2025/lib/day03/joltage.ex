8b358c15f564df7847ae5ce808c69307c7b159ca
vicho1822 <javitonogueira@gmail.com>
2025-12-03 19:59:50 +0100

    feat: part 2 day 3 might be working

--- advent_of_code_2025/lib/day03/joltage.ex
+++ advent_of_code_2025/lib/day03/joltage.ex
@@ -18,8 +18,8 @@ defmodule Joltage do
   defp loop(sum) do
     receive do
       {:bank, lst} ->
-        {f, l} = get_bank_joltage(lst)
-        num = f * 10 + l
+        tol = get_bank_joltage(lst, 12)
+        num = sum_batt(tol, 0)
         loop(sum + num)
       {:EOF, from} ->
         send(from, {:joltage, sum})
@@ -29,22 +29,46 @@ defmodule Joltage do
     end
   end
 
-  defp get_bank_joltage(lst), do: get_bank_joltage(lst, -1, -1)
-  defp get_bank_joltage([num], fnum, lnum) do
-    case num > lnum do
-      true -> {fnum, num}
-      false -> {fnum, lnum}
+  def get_bank_joltage(lst, min_length), do: get_bank_joltage(lst, -1, [], min_length)
+  defp get_bank_joltage([], fnum, rest, _min_length), do: [fnum | rest]
+  defp get_bank_joltage([num | lst], fnum, rest, min_length) do
+    case num > fnum do
+      true ->
+        cond do
+          length(lst) + 1 > min_length ->
+            get_bank_joltage(lst, num, [], min_length)
+          length(lst) + 1 == min_length ->
+            lst   # Si llegamos a tener length + 1 == 12 y num > fnum entonces ya es el número más alto
+          true ->
+            new_rest = update_rest(num, rest, min_length - 1, length(lst) + 1, min_length)
+            get_bank_joltage(lst, fnum, new_rest, min_length)
+        end
+      false ->
+        new_rest = update_rest(num, rest, min_length - 1, length(lst) + 1, min_length)
+        get_bank_joltage(lst, fnum, new_rest, min_length)
     end
   end
-  defp get_bank_joltage([num | lst], fnum, lnum) do
-    case num > fnum do
-      true -> get_bank_joltage(lst, num, -1)
-      false ->
-        case num > lnum do
-          true -> get_bank_joltage(lst, fnum, num)
-          false -> get_bank_joltage(lst, fnum, lnum)
-        end
-    end
+
+  defp update_rest(_num, lst, 0, _remaining, _needed), do: lst
+  defp update_rest(num, [], _min_length, _remaining, _needed), do: [num]
+  defp update_rest(num, [fnum | rest], min_length, remaining, needed) do
+      case min_length <= remaining do
+        true ->
+          case num > fnum do
+            true ->
+              [num]
+            false ->
+              [fnum | update_rest(num, rest, min_length - 1, remaining, needed)]
+          end
+        false ->
+          [fnum | update_rest(num, rest, min_length-1, remaining, needed)]
+      end
+  end
+
+  defp sum_batt([], res), do: res
+  defp sum_batt([f | lst], res) do
+    n = f * :math.pow(10, length(lst))
+    sum_batt(lst, res + n)
   end
 
 end
