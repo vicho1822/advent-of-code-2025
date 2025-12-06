73005b0b06457e8522a2c62cf6a6d7dbeba3a622
vicho1822 <javitonogueira@gmail.com>
2025-12-06 22:33:31 +0100

    feat: part 2 day 06 : 'cephalopod sintax'

--- advent_of_code_2025/lib/day06/cephalopod_math.ex
+++ advent_of_code_2025/lib/day06/cephalopod_math.ex
@@ -4,10 +4,78 @@ defmodule Cephalopod_math do
   """
   @operations ["*", "+"]
 
+  def ceph_solve(input) do
+    {op, nums} = parse_in_ceph_sintax(input)
+    operate(nums, op)
+    |> Enum.sum()
+  end
+
   def solve_problem(input) do
     parse(input)
     |> calc()
     |> Enum.sum()
+  end
+
+  defp parse_in_ceph_sintax(input) do
+    File.read!(input)
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn str ->
+      String.split(str, "", trim: true)
+    end)
+    |> tupling_numbers()
+  end
+
+  defp tupling_numbers(mtx) do
+    operators = Enum.at(mtx, length(mtx) - 1)
+    |> Enum.reject(fn elem -> elem == " " end)
+
+    nums_lists = Enum.reject(mtx, fn lst ->
+      Enum.at(lst, 0) in @operations
+    end)
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(fn lst ->
+      get_digits(lst)
+    end)
+
+    {operators, nums_lists}
+  end
+
+  defp get_digits(lst) do
+    indexed = Enum.map(lst, fn num ->
+      case num == " " do
+        true -> []
+        false -> String.to_integer(num)
+      end
+    end)
+    |> Enum.reject(fn empty -> empty == [] end)
+    |> Enum.with_index()
+
+    Enum.map(indexed, fn {num, pos} ->
+      potency = length(indexed) - 1 - pos
+      num * :math.pow(10, potency)
+    end)
+    |> Enum.sum()
+  end
+
+  defp operate(nums, ops), do: operate(nums, ops, [], 0)
+
+  defp operate([], _op, res, acc), do: [acc | res]
+
+  defp operate([0 | nums], [_op | operations], res, acc), do: operate(nums, operations, [acc | res], 0)
+
+  defp operate([n | nums], [op | operations], res, acc) do
+    # case n == 0 do
+    #   true -> operate(nums, operations, [acc | res], 1)
+    #   false ->
+        cond do
+          op == Enum.at(@operations, 0) ->
+            case acc == 0 do
+              true -> operate(nums, [op | operations], res, n)
+              false -> operate(nums, [op | operations], res, acc * n)
+            end
+          op == Enum.at(@operations, 1) -> operate(nums, [op | operations], res, acc + n)
+        end
   end
 
   defp parse(input) do
